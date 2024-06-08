824932b1c9a38fad69d51def5e0e4a0d530623d6
Diana Sosnovskaya <progdidi@gmail.com>
2024-06-08 21:40:23 +0300

    feat: add header, hamburger menu

---
+++ app/js/main.js
@@ -0,0 +1,8 @@
+//hamburger menu
+
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu__list');
+
+menuBtn.addEventListener('click', () => {
+    menu.classList.toggle('active');
+})
